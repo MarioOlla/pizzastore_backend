8e6ba7af86e86f830291ba3b344a32e201dfb9f5
MarioOlla <mario.olla25@gmail.com>
2022-12-23 16:12:02 +0100

    Adattate le dipendenze del pom.xml e aggiunte alcune properties

--- src/main/java/it/prova/pizzastore_backend/PizzastoreBackendApplication.java
+++ src/main/java/it/prova/pizzastore_backend/PizzastoreBackendApplication.java
@@ -1,13 +1,19 @@
 package it.prova.pizzastore_backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PizzastoreBackendApplication {
+public class PizzastoreBackendApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PizzastoreBackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
 	}
 
 }
